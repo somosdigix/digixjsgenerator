c3d92dbb390a6fa7e134cd7d3183614c680df37a
Renan S Moreira <renan.siravegna@gmail.com>
2017-06-26 11:26:54 -0400

    Enriquecendo os testes de unidade do feed

--- app/templates/test/exemplo/feed.module.test.js
+++ app/templates/test/exemplo/feed.module.test.js
@@ -6,22 +6,12 @@ import dom from '~/base-lib/dom';
 import Feed from '~/controllers/exemplo/feed/feed.module';
 
 describe('Feed', () => {
-  let _sandbox, _feed;
+  let _sandbox, _feed, _feeds;
   
   beforeEach(() => {
     dom('body').empty().append('<div data-js="feed"></div>');
 
-    _sandbox = new Sandbox();
-    _feed = new Feed(_sandbox);
-  });
-
-  afterEach(() => {
-    _feed.finalizar();
-    dom('div[data-js="feed"]').remove();
-  });
-
-  it('deve listar as postagens', () => {
-    const feeds = [
+    _feeds = [
       {
         id: 1,
         nome: 'Feed 1',
@@ -33,13 +23,44 @@ describe('Feed', () => {
         mensagem: 'Mensagem 2'
       }
     ];
-    _sandbox.api.feed.obterTodos = () => new FakePromise(feeds);
 
+    _sandbox = new Sandbox();
+    _sandbox.api.feed.obterTodos = () => new FakePromise(_feeds);
+
+    _feed = new Feed(_sandbox);
+  });
+
+  afterEach(() => {
+    _feed.finalizar();
+    dom('div[data-js="feed"]').remove();
+  });
+
+  it('deve listar as postagens', () => {
     _feed.iniciar();
 
     expect(dom('div[data-id="1"]').any()).to.be.ok();
     expect(dom('div[data-id="2"]').any()).to.be.ok();
   });
 
-  it('deve exibir um feedback quando não há postagens a serem exibidas');
+  it('deve exibir um feedback quando não há postagens a serem exibidas', () => {
+    _sandbox.api.feed.obterTodos = () => new FakePromise([]);
+
+    _feed.iniciar();
+
+    expect(dom('p').hasClass('esconder')).to.be.ok();
+  });
+
+  it('não deve exibir um feedback quando houver postagens a serem exibidas', () => {
+    _feed.iniciar();
+
+    expect(dom('p').hasClass('esconder')).not.to.be.ok();
+  });
+
+  it('deve limpar o conteúdo do elemento alvo ao finalizar', () => {
+    _feed.iniciar();
+
+    _feed.finalizar();
+
+    expect(dom('div[data-js="feed"]').getHtml()).to.be.empty();
+  });
 });
